32f7fadec2f6d68d4d12f2bef580f52e5b83883d
Ilya Ryazanov <ilya.ryazanov@quipu.de>
2021-09-05 17:23:37 +0300

    web client MVC

---
+++ WebClient/Models/OrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SPAWebClient.Models
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string Company { get; set; }
+        public string Customer { get; set; }
+        public string Minister { get; set; }
+        public string Email { get; set; }
+    }
+}
